3590ab5de8afb2c27153d506577b81195cd475dd
Victor Amaro <dev.vprazeres@gmail.com>
2025-10-15 22:06:16 -0300

    feat: animating enemies

--- Game_01/src/com/pixelaprazo/entities/Enemy.java
+++ Game_01/src/com/pixelaprazo/entities/Enemy.java
@@ -11,12 +11,19 @@ import com.pixelaprazo.world.World;
 
 public class Enemy extends Entity{
 	
-	private double speed = 1;
+	private double speed = 0.4;
 	
 	private int maskx = 8,masky = 8, maskw = 10, maskh = 10;
+	
+	private int frames = 0,maxFrames = 20,index = 0,maxIndex = 1;
+	
+	private BufferedImage[] sprites;
 
 	public Enemy(int x, int y, int width, int height, BufferedImage sprite) {
-		super(x, y, width, height, sprite);
+		super(x, y, width, height, null);
+		sprites = new BufferedImage[2];
+		sprites[0] = Game.spritesheet.getSprite(112, 16, 16, 16);
+		sprites[1] = Game.spritesheet.getSprite(112+16, 16, 16, 16);
 	}
 	
 	public void tick() {
@@ -33,6 +40,17 @@ public class Enemy extends Entity{
 		else if((int)y > Game.player.getY() && World.isFree(this.getX(), (int)(y-speed)) && !isColliding(this.getX(), (int)(y-speed))) {
 				y-=speed;
 		}
+		
+
+		frames++;
+		if(frames == maxFrames) {
+			frames = 0;
+			index++;
+			if(index > maxIndex) {
+				index = 0;
+			}
+		}
+		
 	}
 	
 	public boolean isColliding(int xnext, int ynext) {
@@ -52,9 +70,10 @@ public class Enemy extends Entity{
 	}
 	
 	public void render(Graphics g) {
-		super.render(g);
-		g.setColor(Color.blue);
-		g.fillRect(this.getX() + maskx - Camera.x, this.getY() + masky - Camera.y, maskw, maskh);
+		g.drawImage(sprites[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
+		
+//		g.setColor(Color.blue);
+//		g.fillRect(this.getX() + maskx - Camera.x, this.getY() + masky - Camera.y, maskw, maskh);
 	}
 
 }
